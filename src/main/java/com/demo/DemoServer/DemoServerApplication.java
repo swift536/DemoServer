8f6d0376a154bbc59a2e657901e7160510fb0a42
swift536 <njherickson94@gmail.com>
2024-04-15 18:32:15 -0500

    initial project setup

---
+++ src/main/java/com/demo/DemoServer/DemoServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.DemoServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServerApplication.class, args);
+	}
+
+}
